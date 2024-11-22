4d73b86a1ee976d9f98112b01b504e777ea48036
shifu-alt <7854@cc.edu.pk>
2024-11-22 21:02:22 +0500

    Test

--- cinema.cpp
+++ cinema.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    cout<<"Testing"<<endl;
-    cout<<"Rainbow kitten surprise"<<endl;
-    cout<<"When it lands -"<<endl;
-    cout<<"is it working?";
-    cout<<"why aint it working?";
-    return 0;
-}
+class movies{
+private: 
+    string title;
+    string genre;
+    float rating;
+    vector<string> showtimes;
+
+
+
+};
